33e163d3d80ddbcd53f1fd58c78143837aedc1c5
Riya Disawal <40329374+Riyayrd@users.noreply.github.com>
2024-12-09 07:58:58 +0530

    Create remove_duplicates.py
    https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/

---
+++ remove_duplicates.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+ def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        j = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[j] = nums[i]
+                j += 1
+        return j       
+        
